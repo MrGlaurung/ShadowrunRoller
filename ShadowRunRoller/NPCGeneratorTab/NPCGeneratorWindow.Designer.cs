17783c345904bfddd31de508f356ad8584411188
Anders Liden <devel@liden.cx>
2019-01-23 09:50:26 +0100

    Trying to build an image button.

--- ShadowRunRoller/NPCGeneratorTab/NPCGeneratorWindow.Designer.cs
+++ ShadowRunRoller/NPCGeneratorTab/NPCGeneratorWindow.Designer.cs
@@ -80,6 +80,8 @@
             this.BodyStatTextBox = new System.Windows.Forms.TextBox();
             this.BodyStatLabel = new System.Windows.Forms.Label();
             this.InitiativeGroupBox = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.InitiativeGroupBox.SuspendLayout();
@@ -195,6 +197,8 @@
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.InitiativeGroupBox);
             this.groupBox2.Controls.Add(this.MoveTextBox);
             this.groupBox2.Controls.Add(this.MoveLabel);
@@ -671,6 +675,7 @@
             // 
             // InitiativeGroupBox
             // 
+            this.InitiativeGroupBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.InitiativeGroupBox.Controls.Add(this.AstralInitiativeTextBox);
             this.InitiativeGroupBox.Controls.Add(this.InitiativeLabel);
             this.InitiativeGroupBox.Controls.Add(this.MatrixInitiativeLabel);
@@ -683,6 +688,23 @@
             this.InitiativeGroupBox.TabIndex = 42;
             this.InitiativeGroupBox.TabStop = false;
             this.InitiativeGroupBox.Text = "Initiative";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Full skilltree";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 44);
+            this.button1.TabIndex = 44;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // NPCGeneratorWindow
             // 
@@ -757,5 +779,7 @@
         private System.Windows.Forms.TextBox MemoryTextBox;
         private System.Windows.Forms.Label MemoryLabel;
         private System.Windows.Forms.GroupBox InitiativeGroupBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
